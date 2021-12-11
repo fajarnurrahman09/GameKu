82ec8e6ee904b47f991d1fe8805fd13fcf88b403
fajarnurrahman09 <62900256+fajarnurrahman09@users.noreply.github.com>
2021-12-12 01:53:47 +0700

    feat: list of game

--- app/src/main/java/com/example/gameku/Game.kt
+++ app/src/main/java/com/example/gameku/Game.kt
@@ -3,7 +3,9 @@ package com.example.gameku
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+
 @Parcelize
+
 data class Game (
    val imgGame: Int,
    val nameGame: String,
--- app/src/main/java/com/example/gameku/MainActivity.kt
+++ app/src/main/java/com/example/gameku/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.gameku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gameList = listOf<Game>(
+            Game(
+                R.drawable.valorant,
+                nameGame = "Valorant",
+                descGame = "lorem lorem lorem"
+            )
+        )
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_game)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = GameAdapter(this, gameList){
+
+        }
+
     }
 }
